bf42b9d063d096efb225172293fcd6960d126bda
Kawã Sousa <kawasousacontato@gmail.com>
2025-01-29 18:24:06 -0300

    :sparkles: feat: Adiciona componente react da página de registro

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,13 +1,57 @@
 import './style.css'
+import { useState } from "react";
+import { Link } from 'react-router-dom';
+import RoundContainer from '../../components/RoundContainer'
+import LogoContainer from "../../components/LogoContainer";
 
 function SignUp() {
-  return (
-    <div>
-      <h1>
-        Hello RebirthIt!
-      </h1>
-    </div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-export default SignUp
+  function handleSignUp() {
+    logginUser(username, password);
+  }
+
+  return (
+    <div className="main-container">
+      <LogoContainer />
+      <h2>Cadastre-se</h2>
+      <form>
+        <RoundContainer>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            required
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </RoundContainer>
+        <RoundContainer>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </RoundContainer>
+        <RoundContainer>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </RoundContainer>
+        <RoundContainer>
+          <button onClick={handleSignUp}>SignUp</button>
+        </RoundContainer>
+      </form>
+      <Link to={"/auth/login"}>Já tem uma conta? Faça login!</Link>
+    </div>
+  );
+};
+
+export default SignUp;
