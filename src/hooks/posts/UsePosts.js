62d0c7ad1611396be7b216cfaa770ad18433b17d
Kawã Sousa <kawasousacontato@gmail.com>
2025-01-29 18:21:24 -0300

    :sparkles: feat: Adiciona hook de get para posts

---
+++ src/hooks/posts/UsePosts.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { getAllPosts } from "../../services/PostService";
+
+export function usePosts() {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const data = await getAllPosts();
+                setPosts(data);
+            } catch (err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchData();
+    }, [])
+
+    return { posts, loading, error }
+}
