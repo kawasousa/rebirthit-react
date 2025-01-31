b0976612f0b6b6021e9c2dfe6a49b5cec9476090
Kawã Sousa <kawasousacontato@gmail.com>
2025-01-31 19:26:32 -0300

    :sparkles: feat: Adiciona componente com os botões de seleção de ícone na página de registro

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,22 +1,31 @@
 import './style.css'
+import '../../assets/icons/profile-icons'
 import { useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import RoundContainer from '../../components/RoundContainer'
 import LogoContainer from "../../components/LogoContainer";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { createProfile } from '../../services/authService'
 
 function SignUp() {
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
-  function handleSignUp() {
-    logginUser(username, password);
+  const profileIcons = ["user-astronaut", "mug-saucer", "user-graduate", "user-ninja"];
+  const [selectedIcon, setSelectedIcon] = useState(null);
+
+  function handleSignUp(event) {
+    event.preventDefault();
+    createProfile(navigate, username, email, password, name, selectedIcon);
   }
 
   return (
     <div className="main-container">
       <LogoContainer />
-      <h2>Cadastre-se</h2>
+      <h2>{`Boas vindas${!name? '!' : ', ' + name + '!'}`}</h2>
       <form>
         <RoundContainer>
           <input
@@ -38,18 +47,42 @@ function SignUp() {
         </RoundContainer>
         <RoundContainer>
           <input
-            type="password"
-            placeholder="Password"
+            type="text"
+            placeholder="Senha"
             value={password}
             required
             onChange={(e) => setPassword(e.target.value)}
           />
         </RoundContainer>
         <RoundContainer>
-          <button onClick={handleSignUp}>SignUp</button>
+          <input
+            type="text"
+            placeholder="Seu nome"
+            value={name}
+            required
+            onChange={(e) => setName(e.target.value)}
+          />
         </RoundContainer>
+        <p>seu ícone</p>
+        <RoundContainer extraClasses={"icons-container"} >
+          {
+            profileIcons.map((icon, index) => {
+              return (
+                <button key={index} type='button'
+                  className={`selected-icon ${selectedIcon === icon ? 'active' : ''}`}
+                  onClick={() => setSelectedIcon(icon)}
+                >
+                  <FontAwesomeIcon icon={icon} size='2x' />
+                </button>
+              )
+            })
+          }
+        </RoundContainer>
+        <button className='submit-button' onClick={handleSignUp}>
+          SignUp
+        </button>
       </form>
-      <Link to={"/auth/login"}>Já tem uma conta? Faça login!</Link>
+      <Link to={"/"}>Já tem uma conta? Faça login!</Link>
     </div>
   );
 };
