08c3d49dd7dee0e02e99feb14d63cf5f7f4be04a
Kawã Sousa <kawasousacontato@gmail.com>
2025-02-02 08:28:28 -0300

    :sparkles: feat: Adiciona e estiliza página base de erro

---
+++ src/pages/ErrorPage/index.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '../../assets/icons/project-icons'
+
+function ErrorPage() {
+    return (
+        <div>
+            <FontAwesomeIcon icon="road-barrier" />
+        </div>
+    )
+}
+
+export default ErrorPage;
