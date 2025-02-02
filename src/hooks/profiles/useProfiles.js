90f1b79a60ae592ef1bc9ba3b61c5e8a4db54c53
Kawã Sousa <kawasousacontato@gmail.com>
2025-02-02 08:17:59 -0300

    :sparkles: feat: Adiciona hook de profile

---
+++ src/hooks/profiles/useProfiles.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import { getAllProfiles } from "../../services/ProfileService";
+
+export function useProfiles() {
+    const [profiles, setProfiles] = useState([]);
+    const [profilesLoading, setLoading] = useState(true);
+    const [profilesError, setError] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const data = await getAllProfiles();
+                setProfiles(data);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchData();
+    }, []
+    )
+
+    return { profiles, profilesLoading, profilesError }
+}
