3ba62d3bd5cb3d6992973d58e938bb2b8434547b
Kawã Sousa <kawasousacontato@gmail.com>
2025-02-09 23:53:00 -0300

    :sparkles: feat: Adicionar novos ícones FontAwesome para interações e reações nos posts

--- src/assets/icons/project-icons.js
+++ src/assets/icons/project-icons.js
@@ -1,8 +1,10 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCircleNotch, faTriangleExclamation, faMagnifyingGlass, faRoadBarrier, faCircleCheck, faTrash, faPersonCircleQuestion, faFilter, faFilterCircleXmark, faHandsHoldingCircle } from "@fortawesome/free-solid-svg-icons"
+import { faCircleNotch, faTriangleExclamation, faMagnifyingGlass, faRoadBarrier, faCircleCheck, faTrash, faPersonCircleQuestion, faFilter, faFilterCircleXmark, faHandsHoldingCircle, faHeartCircleXmark, faHeartCircleCheck, faThumbsUp, faThumbsDown, faFaceGrinSquintTears, faFaceSurprise } from "@fortawesome/free-solid-svg-icons"
 
 library.add(
     faHandsHoldingCircle,
     faCircleNotch, faTriangleExclamation, faMagnifyingGlass, faRoadBarrier,
-    faCircleCheck, faTrash, faPersonCircleQuestion, faFilter, faFilterCircleXmark
+    faCircleCheck, faTrash, faPersonCircleQuestion, faFilter, faFilterCircleXmark,
+    faHeartCircleXmark, faHeartCircleCheck,
+    faThumbsUp, faThumbsDown, faFaceGrinSquintTears, faFaceSurprise
 );
